6c10acdba40c530a78c8c2037777c4a4168842cc
Marc Pous <marc.pous@gmail.com>
2024-06-17 14:07:48 +0200

    llamafile running on balena

--- download_script.sh
+++ download_script.sh
@@ -3,16 +3,26 @@
 # File name
 FILE_NAME="llamafile-server-0.1-llava-v1.5-7b-q4"
 
+echo "Starting ... "
+
 # Check if the file already exists
 if [ ! -f "/data/$FILE_NAME" ]; then
+	echo "Downloading the file...."
 	    # Download the file
-	        wget -O "/data/$FILE_NAME" https://huggingface.co/jartine/llava-v1.5-7B-GGUF/resolve/main/llamafile-server-0.1-llava-v1.5-7b-q4
+	        wget -O "/data/$FILE_NAME" https://huggingface.co/Mozilla/llava-v1.5-7b-llamafile/resolve/0bb382b03622305a4e383f0eafce7915404fbd0c/llamafile-server-0.1-llava-v1.5-7b-q4
+else
+	echo "The $FILE_NAME is already downloaded at the /data/ folder..."
 fi
+
+echo "Make the binay executable..."
 
 # Make the binary executable
 chmod 755 "/data/$FILE_NAME"
 
 # Execute the binary
 # sh -c for if mac has zsh issues
-sh -c "/data/$FILE_NAME --host 0.0.0.0"
 
+echo "Starting the $FILE_NAME..."
+
+sh -c "/data/$FILE_NAME --host 0.0.0.0"
+#sleep infinity
